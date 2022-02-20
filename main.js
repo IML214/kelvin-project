976a18724b8a7b387163f0b020ce01ca90420592
IML214 <99942214+IML214@users.noreply.github.com>
2022-02-19 19:26:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+// The forecast today is 293 Kelvin. 293 Kelvin is a constant.
 const kelvin = 293;
+
+// Celsius is 273 degrees less than Kelvin. Find the Celsius.
+var celsius = kelvin - 293;
+
+//After finding the Celsius, find the Fahrenheit.
+var fahrenheit = celsius * (9/5) + 32;
+
+//The answer in Fahrenheit is usually in decimal form. Find the decimal.
+fahrenheit = Math.floor(fahrenheit));
+
+console.log(`The temperature is ${fahrenheit} degrees in Fahrenheit.`);
+
+// What's 0 Kelvin in Fahrenheit?
+var kelvin = 0;
+
+var celsius = var kelvin - 293;
+
+var fahrenheit = celsius * (9/5) + 32;
+
+fahreneit
